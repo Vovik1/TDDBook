8470335aeacf8297bc7bb5710edc5c007b11ff6c
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-08-09 21:30:54 +0300

    Add gunicorn to virtualenv requirements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,9 +85,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-
+        time.sleep(1)
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
             inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
         )
-
