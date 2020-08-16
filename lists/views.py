4f51c5b44cfe80e01970c6e0bd00428c00362d5b
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-08-16 13:55:09 +0300

    enforce model validation in list view

--- lists/views.py
+++ lists/views.py
@@ -9,8 +9,18 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, "list.html", {"list": list_})
+    error = None
+
+    if request.method == "POST":
+        try:
+            item = Item(text=request.POST["item_text"], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect("/lists/{}/".format(list_.id))
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, "list.html", {"list": list_, "error": error})
 
 
 def new_list(request):
@@ -21,12 +31,7 @@ def new_list(request):
         item.save()
     except ValidationError:
         list_.delete()
-        error = "You can't have an empty list item" 
-        return render(request, 'home.html', {'error': error})
+        error = "You can't have an empty list item"
+        return render(request, "home.html", {"error": error})
     return redirect("/lists/{}/".format(list_.id))
 
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST["item_text"], list=list_)
-    return redirect("/lists/{}/".format(list_.id))
