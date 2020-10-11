9d5982ecf32725289f9783a2508c57f83401924b
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-10-11 21:51:41 +0300

    add list owners via forms. more isolated tests

--- lists/models.py
+++ lists/models.py
@@ -1,10 +1,26 @@
 from django.urls import reverse
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class List(models.Model):
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE
+    )
+
     def get_absolute_url(self):
         return reverse("view_list", args=[self.id])
+
+    @staticmethod
+    def create_new(first_item_text, owner=None):
+        list_ = List.objects.create(owner=owner)
+        Item.objects.create(text=first_item_text, list=list_)
+        return list_
+
+    @property
+    def name(self):
+        return self.item_set.first().text
 
 
 class Item(models.Model):
@@ -12,8 +28,8 @@ class Item(models.Model):
     list = models.ForeignKey(List, models.SET_NULL, blank=True, null=True)
 
     class Meta:
-        ordering = ('id',)
-        unique_together = ('list', 'text')
+        ordering = ("id",)
+        unique_together = ("list", "text")
 
     def __str__(self):
         return self.text
